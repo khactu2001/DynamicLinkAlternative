f8b5a51256e5272a3b1b63bad93cd877470ef68e
BM Lee <tulk@brickmate.kr>
2023-09-11 17:28:14 +0700

    config dynamic link

--- index.js
+++ index.js
@@ -5,5 +5,23 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+// import appsFlyer from 'react-native-appsflyer';
+
+// appsFlyer.initSdk(
+//   {
+//     devKey: 'XSMue2TmFzXo5NEoTGTSQh',
+//     isDebug: true,
+//     //   appId: '41*****44',
+//     onInstallConversionDataListener: true, //Optional
+//     onDeepLinkListener: true, //Optional
+//     timeToWaitForATTUserAuthorization: 10, //for iOS 14.5
+//   },
+//   result => {
+//     console.log(result);
+//   },
+//   error => {
+//     console.error(error);
+//   },
+// );
 
 AppRegistry.registerComponent(appName, () => App);
