752414bc14670d9e662bd3968e03ab210c96710f
BM Lee <tulk@brickmate.kr>
2023-09-18 09:24:36 +0700

    insert bottom bar

--- App.tsx
+++ App.tsx
@@ -1,48 +1,6 @@
 import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import FeedScreen from '~modules/feed/Feed';
-import SettingsScreen from '~modules/settings/Settings';
-import {RootStackParamList} from '~navigation/types';
-import HomeScreen from '~modules/home/Home';
-import {Linking, Text} from 'react-native';
+import NavigationTree from '~navigation/Navigation';
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
 export default function App() {
-  const config = {
-    screens: {
-      FeedScreen: 'feed/:userId',
-      SettingsScreen: 'setting',
-    },
-  };
-  const linking = {
-    prefixes: [
-      'dynamiclinkalternative://',
-      'https://android-dla.onelink.me/toL9/omigpmkk',
-    ],
-    config,
-  };
-  Linking.getInitialURL().then(url => {
-    console.log('url', url);
-  });
-  return (
-    <NavigationContainer
-      linking={linking}
-      fallback={<Text>Loading...</Text>}
-      onStateChange={state => console.log('New state is', state?.routes)}>
-      <Stack.Navigator
-        screenOptions={{
-          // headerShown: false,
-          headerBackTitleVisible: false,
-          headerStyle: {
-            backgroundColor: '#6200EE',
-          },
-          headerTintColor: '#fff',
-        }}>
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        <Stack.Screen name="FeedScreen" component={FeedScreen} />
-        <Stack.Screen name="SettingsScreen" component={SettingsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <NavigationTree />;
 }
---
+++ src/navigation/Navigation.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import FeedScreen from '~modules/feed/Feed';
+import SettingsScreen from '~modules/settings/Settings';
+import {RootStackParamList} from '~navigation/types';
+import HomeScreen from '~modules/home/Home';
+import {Linking, Text} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Home from '~modules/home/Home';
+import CustomBottomBar from '~sharedComponent/bottom-bar/CustomBottomBar';
+
+const BottomTab = createBottomTabNavigator();
+
+const NativeStack = createNativeStackNavigator<RootStackParamList>();
+export default function NavigationTree() {
+  const config = {
+    screens: {
+      FeedScreen: 'feed/:userId',
+      SettingsScreen: 'setting',
+    },
+  };
+
+  const linking = {
+    prefixes: [
+      'dynamiclinkalternative://',
+      'https://android-dla.onelink.me/toL9/omigpmkk',
+    ],
+    config,
+  };
+
+  Linking.getInitialURL().then(url => {
+    console.log('url', url);
+  });
+
+  return (
+    <NavigationContainer
+      linking={linking}
+      fallback={<Text>Loading...</Text>}
+      onStateChange={state => console.log('New state is', state?.routes)}>
+      {/* <NativeStack.Navigator
+        screenOptions={{
+          // headerShown: false,
+          headerBackTitleVisible: false,
+          headerStyle: {
+            backgroundColor: '#6200EE',
+          },
+          headerTintColor: '#fff',
+        }}>
+        <NativeStack.Screen name="HomeScreen" component={HomeScreen} />
+        <NativeStack.Screen name="FeedScreen" component={FeedScreen} />
+        <NativeStack.Screen name="SettingsScreen" component={SettingsScreen} />
+      </NativeStack.Navigator> */}
+
+      {/* bottom tab */}
+      <BottomTab.Navigator tabBar={props => <CustomBottomBar {...props} />}>
+        <BottomTab.Screen name="HomeScreen" component={HomeScreen} />
+        <BottomTab.Screen name="SettingsScreen" component={SettingsScreen} />
+      </BottomTab.Navigator>
+    </NavigationContainer>
+  );
+}
