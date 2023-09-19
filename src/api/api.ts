bb5ef75c20e948833703610d30dae65f4eb3b5dc
LeeMac <tulk@brickmate.kr>
2023-09-19 22:51:28 +0700

    custom axios get

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import axios, {InternalAxiosRequestConfig} from 'axios';
+import axios, {AxiosHeaders, AxiosRequestConfig} from 'axios';
 
 const BASE_URL = process.env.API_IMAGES;
 const CLIENT_ID = process.env.CLIENT_IDL;
@@ -7,37 +7,32 @@ const CLIENT_ID = process.env.CLIENT_IDL;
 //   baseURL: BASE_URL,
 //   timeout: 5000,
 // });
+const config = {
+  baseURL: BASE_URL,
+  timeout: 5000,
+};
 
-class AxiosService {
-  instance = axios.create({
-    baseURL: BASE_URL,
-    timeout: 5000,
-  });
+type TParams = {
+  [key: string]: string;
+};
+export default class AxiosService {
+  private requestConfig = async (props: AxiosRequestConfig) => {
+    try {
+      return await axios.request({
+        ...props,
+        baseURL: BASE_URL,
+      });
+    } catch (err) {
+      // handle error here
+      console.error(err);
+    }
+  };
+
+  get = (pathname: string, headers?: AxiosHeaders, params?: TParams) => {
+    return this.requestConfig({
+      url: pathname,
+      headers,
+      params,
+    });
+  };
 }
-
-// async function AxiosService() {
-//   const instance = axios.create({
-//     baseURL: BASE_URL,
-//     timeout: 5000,
-//   });
-
-//   const accessToken = await AsyncStorage.getItem('accessToken');
-
-//   if (accessToken) {
-//     instance.interceptors.request.use(
-//       (config: InternalAxiosRequestConfig<any>) => {
-//         config.headers.Authorization = `Bearer ${accessToken}`;
-//         return config;
-//       },
-//       (error: any) => {
-//         return Promise.reject(error);
-//       },
-//     );
-//   }
-
-//   // const {get, post, patch} = instance;
-//   return instance;
-//   // return {get, post, patch};
-// }
-
-// export default AxiosService;
--- src/api/home-api.ts
+++ src/api/home-api.ts
@@ -1,12 +1,36 @@
 const HOME_PATHNAME = {
-  GET_IMAGES_URL: '',
+  GET_IMAGES_URL: 'photos/',
+  SEARCH_IMAGES_URL: 'search/',
+  GET_RANDOM_IMAGES_URL: 'photos/random',
 };
 
 import API from './api';
+const api = new API();
 
-class HomeService {
-  getImages = async () => {
-    const results = await API.get(HOME_PATHNAME.GET_IMAGES_URL);
+type TImagesParams = 'page' | 'per_page' | 'order_by';
+type TGetImagesParams = {
+  [Key in TImagesParams]: string;
+};
+
+export default class HomeService {
+  getImages = async (props: TGetImagesParams) => {
+    const results = await api.get(
+      HOME_PATHNAME.GET_IMAGES_URL,
+      undefined,
+      props,
+    );
+
+    return results;
+  };
+
+  searchImages = async () => {
+    const results = await api.get(HOME_PATHNAME.SEARCH_IMAGES_URL);
+
+    return results;
+  };
+
+  getRandomImages = async () => {
+    const results = await api.get(HOME_PATHNAME.GET_RANDOM_IMAGES_URL);
 
     return results;
   };
